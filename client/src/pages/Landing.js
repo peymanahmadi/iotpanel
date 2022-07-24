7a5cb08e17a2b3938f633436df5b13fc40ecbe77
Peyman Ahmadi <peyman.ahmadi@outlook.com>
2022-07-24 17:58:20 +0430

    [Add]: Landing Page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,4 +1,6 @@
+import main from "../assets/images/main.svg";
 import Logo from "../components/Logo";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
@@ -6,6 +8,25 @@ const Landing = () => {
       <nav>
         <Logo />
       </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            IoT <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime,
+            corporis eveniet. Porro corrupti ad quo facere numquam, quis autem
+            eos accusamus nisi perspiciatis, quisquam eius quae veniam incidunt
+            quia quaerat harum modi recusandae expedita doloribus consequatur.
+            Quidem dolorem distinctio ea modi, qui cupiditate quos in excepturi
+            animi ab,
+          </p>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt="smart home" className="img main-img" />
+      </div>
     </main>
   );
 };
